f8279bbea778e9efbef5a5104216edfd816f762b
Nikolui333 <semenovnikolu@yandex.ru>
2021-07-31 18:44:35 +0300

    start

---
+++ app/src/main/java/com/semenov/sqlitesearch/MainActivity.java
@@ -0,0 +1,15 @@
+package com.semenov.sqlitesearch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInsta
+                nceState);
+        setContentView(R.layout.activity_main);
+    }
+}
